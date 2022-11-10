48114e175ce70949520727204b9eb1a906709b25
Lucas Cejas <cejas_89@hotmail.com>
2022-11-09 23:30:21 -0300

    Desafio 2

--- App.js
+++ App.js
@@ -1,31 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, TouchableOpacity, View, FlatList } from 'react-native';
 
 export default function App() {
-  const [texItem, settextItem] = useState("");
-  const [itemList, setitemList] = useState([]);
-  const [modalVisible, setmodalVisible] = useState(false);
+  const [textItem, setTextItem] = useState("");
+  const [list, setList] = useState([]);
+
+  const onHandleChange = (t) => setTextItem(t);
 
 
+  const addItem = () => {
+    setList((currentState) => [
+      ...currentState,
+      { id: Math.random().toString(), value: textItem },
+    ]);
+    setTextItem("");
+  };
 
-
-
-
-
+  const renderItem = ({ item }) => (
+    <TouchableOpacity onPress={() => selectedItem(item.id)}>
+      <Text style={{color: "red"}}>{item.value}</Text>
+    </TouchableOpacity>
+  );
 
 
 
   return (
     <View style={styles.container}>
-      <View style={styles.tittleContainer}>
-        <Text style={styles.tittle}>Shopping List</Text>
-        <TextInput style={styles.inputContainer} placeholder='ingresa aqui tu informacion'></TextInput>
-      </View>
-      <View style={{ flex: 1 }}>
-        <TouchableOpacity style={styles.buttonContainer}>
-          <Text>Presiona Aqui</Text>
+      <Text style={styles.tittle}>Shopping List</Text>        
+        <View style={styles.inputContainer}>
+        <TextInput style={styles.inputStyle}
+        placeholder='ingresa aqui tu informacion'
+        value={textItem}
+        onChangeText={onHandleChange}
+        >
+        </TextInput>
+        <TouchableOpacity
+        style={styles.buttonContainer}
+        onPress={addItem}>
+          <Text style={{textAlign: 'center', color: "white"}}>Presiona Aqui</Text>
         </TouchableOpacity>
-
+        <FlatList
+          data={list}
+          renderItem={renderItem}
+          keyExtractor={(item) => item.id}
+        />
       </View>
       <StatusBar style="auto" />
     </View>
@@ -37,33 +56,30 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: 'blue',
   },
-  tittleContainer: {
-    flex: 1
-
-
-  },
   tittle: {
-    flex: 1,
     textAlign: 'center',
     fontSize: 50,
-    fontWeight: 'bold'
-
+    fontWeight: 'bold',
+    marginTop: 50,
   },
   inputContainer: {
-    flex: 1,
-    backgroundColor: 'purple',
+    backgroundColor: 'white',
     margin: 10,
     borderColor: 'green',
-    borderWidth: 5,
-
+    borderWidth: 1,
+    alignItems: 'center',
+    
+  },
+  inputStyle: {
+    borderBottomColor: "black",
+    borderBottomWidth: 1,
+    width: 250,
+    textAlign: 'center',
   },
   buttonContainer: {
-    flex: 1,
-    backgroundColor: 'yellow',
+    backgroundColor: 'black',
     margin: 10,
-
+    width: "30%",
+    
   }
-
-
-
 });
